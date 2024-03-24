30eeba4c5424d986c902352e72e744289e9bbabc
Dmitry Monakhov <monakhov@amazon.com>
2024-03-24 17:56:36 +0100

    p4: fix cpuset typo

--- utils/hpl-aws-p4.sh
+++ utils/hpl-aws-p4.sh
@@ -7,5 +7,5 @@ export HPL_USE_NVSHMEM=0
 
 /workspace/hpl.sh \
     --mem-affinity 0:0:0:0:1:1:1:1 \
-    --cpu-affinity 0-3:4-7:8-11:12-15:24-27:28-31:32-35:36-49 \
+    --cpu-affinity 0-3:4-7:8-11:12-15:24-27:28-31:32-35:36-39 \
     $@
